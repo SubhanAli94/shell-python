f66a5d01de68b61db35b563a746eecad6e2aeac9
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-01 17:17:38 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,6 +1,22 @@
 import os
 import sys
+import subprocess
 
+
+def is_builtin(command):
+    builtins = ['echo', 'exit', 'type']
+    return command in builtins
+
+def iterate_paths(command):
+    path_list = os.environ['PATH'].split(os.pathsep)
+    for path in path_list:
+        file_path = os.path.join(path, command)
+        if is_executable(file_path):
+            return file_path
+    return None
+
+def is_executable(file_path):
+    return os.path.isfile(file_path) and os.access(file_path, os.X_OK)
 
 def main():
 
@@ -10,25 +26,28 @@ def main():
         
         if user_input.startswith('type '):
             command = user_input[5:]
-            if command == 'echo' or command == 'exit' or command == 'type':
+            if is_builtin(command):
                 print(f"{command} is a shell builtin")
             else:
-                executable_to_find = command
-                path_list = os.environ['PATH'].split(os.pathsep)
-                for path in path_list:
-                    file_path = os.path.join(path, executable_to_find)
-                    if(os.path.isfile(file_path)):
-                        if(os.access(file_path, os.X_OK)):
-                            print(f"{executable_to_find} is {file_path}")
-                            break
+                file_path = iterate_paths(command)
+                if file_path:
+                    print(f"{command} is {file_path}")
                 else:
                     print(f"{command}: not found")
+        
         else:
             if user_input == 'exit':
                 break
             
             if user_input.startswith("echo "):
                 print(user_input[5:])
+            
+            command = iterate_paths(user_input)
+            if command:
+                arguments = user_input.split()[1:]
+                subprocess.run([command] + arguments)
+
+                print(f"{user_input} is {command}")
             else:
                 print(f"{user_input}: command not found")
 
