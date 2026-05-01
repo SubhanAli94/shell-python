1d6a23f0cb42d965bb7be7b566a3a9e3598a3cc0
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-01 14:56:06 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -6,6 +6,9 @@ def main():
     while True:
         sys.stdout.write("$ ")
         user_input = input()
+        
+        if user_input == 'exit':
+            break
         print(f"{user_input}: command not found")
 
     pass
