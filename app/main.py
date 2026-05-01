fdcf923ff0a13965081764dc1053f211907b40e1
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-01 15:11:51 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -7,13 +7,19 @@ def main():
         sys.stdout.write("$ ")
         user_input = input()
         
-        if user_input == 'exit':
-            break
-        
-        if user_input.startswith("echo "):
-            print(user_input[5:])
+        if user_input.startswith('type '):
+            if user_input[5:] == 'echo' or user_input[5:] == 'exit' or user_input[5:] == 'type':
+                print(f"{user_input[5:]} is a shell builtin")
+            else:
+                print(f"{user_input[5:]}: not found")
         else:
-            print(f"{user_input}: command not found")
+            if user_input == 'exit':
+                break
+            
+            if user_input.startswith("echo "):
+                print(user_input[5:])
+            else:
+                print(f"{user_input}: command not found")
 
     pass
 
