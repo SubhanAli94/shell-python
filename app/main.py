99dc1c25382a238b44142c021442ade7a965f524
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-04 14:00:06 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -129,7 +129,7 @@ def main():
                     elif file_name:
                         write_output_to_file(file_name, p.stdout)
                     else:
-                        print(p.stdout)
+                        print("----", p.stdout)
                 else:
                     print(f"{user_input}: command not found")
                     
