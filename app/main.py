05f74a03cd09be61c09371360f8d596ab599c10b
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-04 21:09:48 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -146,27 +146,9 @@ def main():
                 else:  
                     p = subprocess.run([command] + argl, capture_output=True, text=True)
                     
-                    """
-                     p = subprocess.run([command] + argl, capture_output=True, text=True)
-                    
                     for output, file_name in [(op_file_name, p.stdout), (err_file_name, p.stderr)]:
-                        if stripped := (output or "").strip():
+                        if output and (stripped := output.strip()):
                             write_output_to_file(file_name, stripped) if file_name else print(stripped)
-                    """
-
-                    stripped_err = p.stderr.strip()
-                    stripped_op = p.stdout.strip()
-                    if stripped_err:
-                        if err_file_name:
-                            write_output_to_file(err_file_name, stripped_err)
-                        else:
-                            print(stripped_err)
-
-                    if stripped_op:
-                        if op_file_name and stripped_op:
-                            write_output_to_file(op_file_name, stripped_op)
-                        else:
-                            print(stripped_op)
         
         op_file_name = ""
                     
