42db893a16ebb5d20ec768c59309168afa509a9d
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-05 01:29:00 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -24,30 +24,42 @@ def parse_args(args):
     output = []
     curr = ""
     is_escaped = False
-    file_name = ""
+    o_file_name = ""
     for idx, char in enumerate(args):
+
+        if char == '1' and idx+2 < len(args) and args[idx+1] ==">" and args[idx+2] ==">":
+            idx = idx + 3
+            if not is_escaped and not is_in_quotes and not is_in_double_quotes: 
+                o_file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
+                if curr: output.append(curr)
+                return output, o_file_name, None, 'a'
+        
+        if char == '>' and idx+1 < len(args) and args[idx+1] ==">":
+            idx = idx + 2
+            if not is_escaped and not is_in_quotes and not is_in_double_quotes: 
+                o_file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
+                if curr: output.append(curr)
+                return output, o_file_name, None, 'a'
 
         if char == '2' and idx+1 < len(args) and args[idx +1] == ">":
             idx = idx + 2
             if not is_escaped and not is_in_quotes and not is_in_double_quotes: 
                 err_file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
                 write_output_to_file(err_file_name, "")
-                return output, None, err_file_name
+                return output, None, err_file_name, 'w'
 
         if char == '1' and idx + 1 < len(args) and args[idx + 1] == ">":
             idx = idx + 2
             if not is_escaped and not is_in_quotes and not is_in_double_quotes:
-                file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
-                # print(f"file name: {file_name}")
+                o_file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
                 if curr: output.append(curr)
-                return output, file_name, None
+                return output, o_file_name, None, 'w'
         elif char == ">":
             idx = idx + 1
             if not is_escaped and not is_in_quotes and not is_in_double_quotes:
-                file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
-                # print(f"file name: {file_name}")
+                o_file_name = "".join([arg for arg in args[idx:].strip() if arg != '"'])
                 if curr: output.append(curr)
-                return output, file_name, None
+                return output, o_file_name, None, 'w'
 
         if char == '\\' and not is_escaped and not is_in_quotes:
             is_escaped = not is_escaped
@@ -74,7 +86,7 @@ def parse_args(args):
 
     output.append(curr)
 
-    return output, file_name, None
+    return output, o_file_name, None, 'w'
 
 def process_type_command(args):
     args = args.split()
@@ -98,16 +110,18 @@ def process_cd_command(arg):
     except FileNotFoundError:
         print(f"cd: {arg}: No such file or directory")
 
-def write_output_to_file(file_name, output):
-    with open(file_name, 'w') as file:
+def write_output_to_file(file_name, output, file_mode = 'w'):
+    with open(file_name, file_mode) as file:
+        output += '\n' if file_mode == 'a' else ''
         file.write(output)
+
 
 def main():
     
     while True:
         sys.stdout.write("$ ")
         user_input = input()
-        parsed_input, op_file_name, err_file_name = parse_args(user_input.strip())
+        parsed_input, op_file_name, err_file_name, file_mode = parse_args(user_input.strip())
         command = parsed_input[0]
         argl = parsed_input[1:]
         args = " ".join(parsed_input[1:])
@@ -118,15 +132,15 @@ def main():
             case 'type':
                 if (output := process_type_command(args)) is not None:
                     file_name = op_file_name or err_file_name
-                    write_output_to_file(file_name, output) if file_name else print(output)
+                    write_output_to_file(file_name, output, file_mode) if file_name else print(output)
                     
             case 'echo':
-                write_output_to_file(op_file_name, args) if op_file_name else print(args)
+                write_output_to_file(op_file_name, args, file_mode) if op_file_name else print(args)
                     
             case 'pwd':
                 output = os.getcwd()
                 file_name = op_file_name or err_file_name
-                write_output_to_file(file_name, output) if file_name else print(output)
+                write_output_to_file(file_name, output, file_mode) if file_name else print(output)
             case 'cd':
                 process_cd_command(args)  
             case _:
@@ -138,7 +152,7 @@ def main():
                     
                     for file_name, output in [(op_file_name, p.stdout), (err_file_name, p.stderr)]:
                         if output and (stripped := output.strip()):
-                            write_output_to_file(file_name, stripped) if file_name else print(stripped)
+                            write_output_to_file(file_name, stripped, file_mode) if file_name else print(stripped)
                     
     pass
 
