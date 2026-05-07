21f88e22b914c1f5b42a5655aa80f0fb2cbf97ab
Ali <22730931+SubhanAli94@users.noreply.github.com>
2026-05-07 11:45:33 +0400

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -57,11 +57,10 @@ def auto_complete(text, state):
         elif len(line.split()) > 1:
             p = os.path.dirname(line.split()[-1]) if "/" in line.split()[-1] else '.'
             matches = get_file_or_dir_matches(text, p)
-            print(f"Matches res: {matches}")
-            
+
         if not matches:
-            print('\x07')
-            return None     
+            print('\x07', end='')
+            return None
 
     try:
         return matches[state]
